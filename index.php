c5bbd70385d4eb5fc581ece1df7bbd0e51b5be4f
dfsfdsfdsfsd <59228985+dfsfdsfdsfsd@users.noreply.github.com>
2020-11-03 12:38:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
 
-	$campaignId = '6ab10w';
+	$campaignId = '6d3ja9';
 	$phpUrl = (is_https() ? "https://" : "http://"). $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 
 	function is_https()
